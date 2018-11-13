86868084b0bf5fd94eb35ce47d627a2999dc60ef
lewisbrotchie <lewis@yulife.com>
2018-11-13 16:37:15 +0000

    Updates

---
+++ precache-manifest.b37afd72b1e72d9229f60183ceae83f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0e0585913e1ba52a3bf",
+    "url": "/translatory/static/js/runtime~main.a0e05859.js"
+  },
+  {
+    "revision": "d187369b77de5c4c8c41",
+    "url": "/translatory/static/js/main.d187369b.chunk.js"
+  },
+  {
+    "revision": "ded6642f72cffddf50cf",
+    "url": "/translatory/static/js/1.ded6642f.chunk.js"
+  },
+  {
+    "revision": "d187369b77de5c4c8c41",
+    "url": "/translatory/static/css/main.2f6f8ec7.chunk.css"
+  },
+  {
+    "revision": "ded6642f72cffddf50cf",
+    "url": "/translatory/static/css/1.0857220b.chunk.css"
+  },
+  {
+    "revision": "28d944b666583aa5281a279ed19d1e1d",
+    "url": "/translatory/index.html"
+  }
+];
